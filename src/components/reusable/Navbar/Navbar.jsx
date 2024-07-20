0f4e8fc2f7578c71ed0cf660e74b0cbca59aaaab
stepheeeen <stephenon56@gmail.com>
2024-07-20 02:00:24 +0100

    creating of the basic components

---
+++ src/components/reusable/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TopNavbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+const BottomNavbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export {TopNavbar, BottomNavbar}
--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import AuthImg from '../assets/images/AuthenticationImg.png'
 
 const Authentication = () => {
   return (
     <div>
+        <div>
+        <img src={AuthImg} alt="alt" className='' />
+        </div>
 
-        
+        <div>
+            <form action=""></form>
+        </div>
     </div>
   )
 }
