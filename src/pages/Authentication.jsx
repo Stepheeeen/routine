a1ea2f4148e3ca57250714882a3e438efc54e9f9
stepheeeen <stephenon56@gmail.com>
2024-07-21 14:15:40 +0100

    responsiveness

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -10,17 +10,18 @@ const Authentication = () => {
     <PageContainer
     navbarStyling={'hidden'}
     pageTitle={'Create Account'}
-    topbarStyling={'flex justify-center items-center'}>
-        <div className='p-2 mt-[-15px]'>
-        <img src={AuthImg} alt="alt" className='' />
+    topbarStyling={'flex justify-center items-center md:justify-start md:pl-[18%]'}
+    className='md:flex md:justify-between md:items-center md:flex-row-reverse'>
+        <div className='p-2 mt-[-15px] md:w-1/2 md:mt-[-85px]'>
+        <img src={AuthImg} alt="alt" className='md:w-[85%]' />
         </div>
 
-        <div className='w-full flex items-center justify-center mt-[25px] pb-2 relative'>
-            <form action="" className='w-[95%] '>
+        <div className='w-full flex items-center justify-center mt-[25px] pb-2 relative md:w-1/2'>
+            <form action="" className='w-[95%] md:w-[60%]'>
               <DefaultInput label={'Username'} customStyling='w-full' />
               <DefaultInput label={'Email'} customStyling='w-full my-[20px]' />
 
-              <DefaultButton text={'Create Account'} variant={'solid'} ButtonStyling={'bg-[#028960] text-[#fff] p-3 rounded-lg absolute bottom-0 w-full mt-[90px]'}/>
+              <DefaultButton text={'Create Account'} variant={'solid'} ButtonStyling={'bg-[#028960] text-[#fff] p-3 rounded-lg absolute bottom-0 w-full mt-[90px] md:mt-[50px]'}/>
             </form>
         </div>
     </PageContainer>
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BottomNavbar, TopNavbar } from '../components/reusable/Navbar/Navbar'
 
-const PageContainer = ({children, navbarStyling, pageTitle, topbarStyling}) => {
+const PageContainer = ({children, navbarStyling, pageTitle, topbarStyling, className}) => {
   return (
-    <div className='h-[100vh]'>
+    <div className={`h-[100vh]`}>
       <TopNavbar pageTitle={pageTitle} topbarStyling={topbarStyling} />
+      <main className={className}>
         {children}
+      </main>
       <BottomNavbar navbarStyling={navbarStyling}  />
     </div>
   )
