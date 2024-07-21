75df0055833c725b43f923bf7023587a210a4d30
stepheeeen <stephenon56@gmail.com>
2024-07-21 16:36:15 +0100

    navbar completed

--- src/components/reusable/Navbar/Navbar.jsx
+++ src/components/reusable/Navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import Home from '../../../assets/icons/homeIcon.svg'
+import HomeActive from '../../../assets/icons/homeIconActive.svg'
+import Task from '../../../assets/icons/taskIcon.svg'
+import TaskActive from '../../../assets/icons/taskIconActive.svg'
+import Calender from '../../../assets/icons/calenderIcon.svg'
+import CalenderActive from '../../../assets/icons/calenderIconActive.svg'
+import Note from '../../../assets/icons/noteIcon.svg'
+import NoteActive from '../../../assets/icons/noteIconActive.svg'
 
-const TopNavbar = ({topbarStyling, pageTitle}) => {
+const TopNavbar = ({ topbarStyling, pageTitle }) => {
   return (
     <div className={`p-2 py-5 ${topbarStyling}`}>
       <h1 className='text-[20px] font-[600]'>{pageTitle}</h1>
@@ -9,12 +17,25 @@ const TopNavbar = ({topbarStyling, pageTitle}) => {
   )
 }
 
-const BottomNavbar = ({navbarStyling}) => {
+const BottomNavbar = ({ navbarStyling }) => {
+  const location = useLocation();
+
   return (
-    <div className={`${navbarStyling}`}>
-      <Link to={'/authentication'}>Signup</Link>
+    <div className={`fixed bottom-0 m-auto w-full p-6 shadow flex items-center justify-between z-10 ${navbarStyling}`}>
+      <Link to='/home'>
+        <img src={location.pathname === '/' ? HomeActive : Home} alt="home" />
+      </Link>
+      <Link to='/task'>
+        <img src={location.pathname === '/task' ? TaskActive : Task} alt="Task" />
+      </Link>
+      <Link to='/calender'>
+        <img src={location.pathname === '/calender' ? CalenderActive : Calender} alt="Calender" />
+      </Link>
+      <Link to='/note'>
+        <img src={location.pathname === '/note' ? NoteActive : Note} alt="Note" />
+      </Link>
     </div>
   )
 }
 
-export {TopNavbar, BottomNavbar}
+export { TopNavbar, BottomNavbar }
